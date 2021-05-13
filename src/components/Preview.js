a5bc5a78ae65242fabbeeb23f14e568b581f18d3
Mat Jordan <mat@utk.edu>
2021-05-12 21:18:33 -0400

    Init preview structure.

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -5,23 +5,36 @@ import Mirador from "./Mirador";
 class Preview extends Component {
 
   render() {
+
+    console.log(this.props.manifest.items[0].items[0].items[0].body[0].service['@id'])
+
     return (
-      <div>
-        <Mirador
-          config={{
-            id: 'mirador',
-            windows: [
-              {
-                loadedManifest: this.props.manifest.id,
-              },
-            ],
-            workspaceControlPanel: {
-              enabled: false,
-            },
-          }}
-          plugins={[]}
-        />
-      </div>
+      <figure className="yith-preview">
+        <div className="yith-preview--body">
+          <figcaption>
+            <strong>{this.props.manifest.label.en[0]}</strong>
+            <p>{this.props.manifest.summary.en[0]}</p>
+            <em>{this.props.manifest.requiredStatement.label.en[0]}: {this.props.manifest.requiredStatement.value.en[0]}</em>
+          </figcaption>
+        </div>
+        <div className="yith-preview--canvas">
+          <img src={this.props.manifest.items[0].items[0].items[0].body[0].service['@id'] + '/full/!300,300/0/default.jpg'} />
+        </div>
+        {/*<Mirador*/}
+        {/*  config={{*/}
+        {/*    id: 'mirador',*/}
+        {/*    windows: [*/}
+        {/*      {*/}
+        {/*        loadedManifest: this.props.manifest.id,*/}
+        {/*      },*/}
+        {/*    ],*/}
+        {/*    workspaceControlPanel: {*/}
+        {/*      enabled: false,*/}
+        {/*    },*/}
+        {/*  }}*/}
+        {/*  plugins={[]}*/}
+        {/*/>*/}
+      </figure>
     )
 
   }

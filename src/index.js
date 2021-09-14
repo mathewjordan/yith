10bed155b7efc1e0ac5834a6660ed7964e2cd95e
Mat Jordan <mat@northwestern.edu>
2021-09-13 21:48:16 -0400

    Add demo, remove tsc generated files.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
-const react_dom_1 = __importDefault(require("react-dom"));
-const yith_1 = __importDefault(require("./yith"));
-react_dom_1.default.render(react_1.default.createElement(yith_1.default, null), document.getElementById('root'));

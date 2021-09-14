51c15e18aaed326f1cae57381dbcb87c8b6488c1
Mat Jordan <mat@northwestern.edu>
2021-09-13 23:12:25 -0400

    Add Expand component placeholder.

---
+++ src/components/Expand.tsx
@@ -0,0 +1,14 @@
+import React, { useCallback, useState } from "react";
+
+interface Props {
+    text: string;
+}
+
+const Expand: React.FC<Props> = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default Expand;

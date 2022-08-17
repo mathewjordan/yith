fd5c282fb6aec3ca8558abc5147323328174d2fa
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-08-17 09:31:11 +0200

    add level0 thumbnail to interstitial preview

--- src/dev.tsx
+++ src/dev.tsx
@@ -10,6 +10,9 @@ const App: React.FC = () => {
         <Yith.Manifest id="https://iiif.harvardartmuseums.org/manifests/object/299837" />
         <Yith.Manifest id="https://ub-iiif.vercel.app/api/manifest/marcus/ubb-bs-fol-00428" />
       </Yith>
+      <Yith type='presentation' preview='interstitial' text='Text'>
+        <Yith.Manifest id='https://ub-iiif.vercel.app/api/manifest/marcus/ubb-ms-0504' />
+      </Yith>
       <Yith type="projection">
         <Yith.Manifest id="https://iiif.harvardartmuseums.org/manifests/object/55652">
           <Yith.Canvas id="https://iiif.harvardartmuseums.org/manifests/object/55652/canvas/canvas-18724207" />
